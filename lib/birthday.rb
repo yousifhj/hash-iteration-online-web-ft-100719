70b5c2355fb3b8d4c27cffd64c8646f5311e7290
Huda Yousif <hjdoski@gmail.com>
2019-10-12 17:37:37 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,11 @@
 # }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
 end
 
 
+
+def age_appropriate_birthday 
